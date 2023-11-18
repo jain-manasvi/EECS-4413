052fed6aad9dd8f1fd17f3460684ac0028dca330
nolan-lobo <140864429+nolan-lobo@users.noreply.github.com>
2023-11-18 12:14:50 -0500

    Add files via upload
    Changed main.js and html to account for filtering and sorting

--- main.js
+++ main.js
@@ -7,3 +7,32 @@ function openTab(tabName) {
     document.getElementById(tabName).style.display = "block"; 
 
   }
+  document.addEventListener("DOMContentLoaded", function() {
+  var sortFilterForm = document.getElementById('sortFilterForm');
+  
+  if (sortFilterForm) {
+      sortFilterForm.addEventListener('submit', function(event) {
+          event.preventDefault(); // Prevent the form from submitting in the traditional way
+          
+          var sortValue = document.getElementById('sort').value;
+          var filterBrandValue = document.getElementById('filterBrand').value;
+
+          // Construct the query parameters
+          var queryParams = '?sort=' + sortValue;
+          if (filterBrandValue) {
+              queryParams += '&brand=' + filterBrandValue;
+          }
+
+          // Make the AJAX call to the backend
+          fetch('/api/products' + queryParams)
+          .then(response => response.json())
+          .then(data => {
+              // Handle the response data
+              console.log(data);
+              // Update the product listing on the page
+              // This would typically involve iterating over the data and appending each product to the DOM
+          })
+          .catch(error => console.error('Error:', error));
+      });
+  }
+});
